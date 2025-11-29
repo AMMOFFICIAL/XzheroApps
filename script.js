120fd9dda7e8128a062123c73fdc5bb0fde1088f
AMMOFFICIAL <akunilham296@gmail.com>
2025-11-29 13:10:39 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //===========[ API GITHUB ]=================//
 
-const API_LOGIN = 'https://api.github.com/repos/AMMOFFICIAL/XzheroApps/contents/github/db.json';
+const API_LOGIN = 'https://raw.githubusercontent.com/AMMOFFICIAL/XzheroApps/refs/heads/main/github/db.json';
 const TOKEN_LOGIN = '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';
 
 const API_BUGS   = 'https://api.github.com/repos/AMMOFFICIAL/XzheroApps/contents/github/sender.json'; 
